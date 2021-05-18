b7928db86a9d27e9cd15db2a9a99f5cf6c1830b8
hussainakram <hussain.akram128@gmail.com>
2021-05-18 13:39:18 +0500

    add gatsby-plugin-gatsby-cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-smoothscroll`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
